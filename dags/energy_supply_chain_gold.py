79fe7a769df35f9e4f239ded0595b9aa838b840a
justnapinn <160308893+justnapinn@users.noreply.github.com>
2026-04-15 17:14:28 +0700

    feat(dag): add gold layer orchestration with automated trigger from silver

---
+++ dags/energy_supply_chain_gold.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Jinnaphat',
+    'start_date': datetime(2026, 4, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'energy_supply_chain_gold',
+    default_args=default_args,
+    schedule=None, 
+    catchup=False,
+    tags=['energy', 'analytics', 'gold', 'pyspark'],
+) as dag:
+
+    start_task = EmptyOperator(task_id='start_gold_layer')
+
+    build_analytics = BashOperator(
+        task_id='build_diesel_analytics',
+        bash_command='python /opt/airflow/dags/scripts/spark/gold_analytics.py',
+    )
+
+    end_task = EmptyOperator(task_id='end_gold_layer')
+
+    start_task >> build_analytics >> end_task
